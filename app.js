1f7812eef14e6ad2ed085f68a677cde237541b6c
Sam Atkins <sam@cubi.io>
2017-03-30 14:43:44 +0100

    feat: Chain commands to enable dynamic weather info

--- app.js
+++ app.js
@@ -2,36 +2,40 @@ const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
+const celcius = require('./helpers/convert');
 
 const argv = yargs
-  .options({
-    address: {
-      demand: true,
-      alias: 'a',
-      describe: 'Fetch weather for this address',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Fetch weather for this address',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 let address = encodeURIComponent(argv.address);
 geocode.geocodeAddress(address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    // pretty print body as JSON with 2 spaces
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
-
-// “Powered by Dark Sky” (linking to https://darksky.net/poweredby/
-// weather.getWeather(lat, lng);
-weather.getWeather(51.5033635,-0.1276248, (errorMessage, weatherResults) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(weatherResults, undefined, 2));
-  }
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        // pretty print body as JSON with 2 spaces
+        // console.log(JSON.stringify(results, undefined, 2));
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                // console.log(JSON.stringify(weatherResults, undefined, 2));
+                console.log('---');
+                console.log(`The temperature is ${celcius.convertFtoC(weatherResults.temperature)} C.`);
+                console.log(`The temperature feels like ${celcius.convertFtoC(weatherResults.apparentTemperature)} C.`);
+                console.log('---');
+                console.log(`Powered by Dark Sky => https://darksky.net/poweredby/`);
+            }
+        });
+    }
 });
