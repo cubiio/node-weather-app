91833ec6eca334e490eefe2871e46c53830f8a00
Sam Atkins <sam@cubi.io>
2017-03-30 11:44:03 +0100

    feat: Add Yargs and set-up CL options

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Fetch weather for this address',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=10%20downing%20street%20london',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  const geo = body.results[0].geometry.location;
+  console.log(`Lat: ${geo.lat} Lng: ${geo.lng}`);
 });
